563ce96a4a21a22007309e0216e8b9a965d9c962
Jamie Morris <jmorris@scottlogic.co.uk>
2017-11-30 11:46:08 +0000

    Fixed navbar logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import {Route, Link } from 'react-router-dom';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem } from 'reactstrap';
 
 import logo from './download.png';
 import './App.css';
@@ -12,7 +12,6 @@ import Form from './components/VoteForm';
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.toggleNavbar = this.toggleNavbar.bind(this);
     this.state = {
       collapsed: true
@@ -32,31 +31,22 @@ class App extends Component {
           <img src={logo} alt="logo" />
           <h1 className="App-title">We are London!</h1>
         </header>
-        <Router>
           <div>
-          <Navbar color="grey" light expand="md">
-            <NavbarBrand href="/">reactstrap</NavbarBrand>
-              <Nav className="ml-auto" navbar>
-                <NavItem>
-                  <Link to="/">Cloud </Link>
-                </NavItem>
-                <NavItem>
-                  <Link to="/vote">Vote Now!</Link>
-                </NavItem>
-                {/* <NavItem>
-                  <Link to="/form">New Vote</Link>
-                </NavItem> */}
+          <Navbar color="faded" light>
+            <NavbarBrand href="/" className="mr-auto">Vote Cloud</NavbarBrand>
+            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+              <Collapse isOpen={!this.state.collapsed} navbar>
+              <Nav navbar>
+                <NavItem><Link to="/">Cloud </Link></NavItem>
+                <NavItem><Link to="/vote">Vote Now!</Link></NavItem>
               </Nav>
+             </Collapse>
            </Navbar>
-            <ul>
-              <li></li>
-            </ul>
             <hr />
             <Route exact path="/" component={Cloud} />
             <Route path="/vote" component={Voter} />
             <Route path="/form" component={Form} />
           </div>
-        </Router>
       </div>
     );
   }
